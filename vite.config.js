e1b07147ce8589f20d12df81991aaa46d1632abe
egemen <egemenylmz01@gmail.com>
2025-11-09 14:20:32 +0300

    fix: Vite base URL ayarı GitHub Pages için düzeltildi

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
+    base: '/goit-markup-hw-09/',
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
